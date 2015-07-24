9f793a4b6bd908e298a7ebb1ce0f386ad9368303
pb <pb@sewm32.(none)>
2015-07-25 03:56:36 +0800

    update xx2ldac.py

--- src/preprocess/mm2ldac.py
+++ src/preprocess/mm2ldac.py
@@ -3,14 +3,15 @@
 #
 
 """
-Convert list-Of-Words file to blei's blei.ldac
-bow.mm in Corpus in the list of Words format
-blei.ldac is Corpus in ldac format
+USAGE: 
+
+convert Gensim bow.mm file to GibbsLDA++ gibbs.mm
+bow.mm in Corpus in the Matrix Market format
+gibbs.mm in Corpus in GibbsLda++ format of List-Of-Words
 
 refer to : http://radimrehurek.com/gensim/corpora/mmcorpus.html
 
-Usage:
-    low2ldac gibbs.low blei.ldac wordids.txt
+Example: python -m lda-test.preprocess.mm2low bow.mm gibbs.low
 
 """
 
@@ -18,7 +19,7 @@ import logging
 import os.path
 import sys
 
-from gensim.corpora import LowCorpus, BleiCorpus, Dictionary
+from gensim.corpora import MmCorpus, BleiCorpus, Dictionary
 
 if __name__ == '__main__':
     program = os.path.basename(sys.argv[0])
@@ -32,12 +33,11 @@ if __name__ == '__main__':
     if len(sys.argv) < 4:
         print(globals()['__doc__'] % locals())
         sys.exit(1)
-    inp, outp, wordids = sys.argv[1:4]
+    inp, outp,dictfile = sys.argv[1:4]
  
     #let's convert the file
-    dictionary = Dictionary.load_from_text(wordids)
-    low = LowCorpus(inp, dictionary)
-    blei = BleiCorpus.serialize(outp, low , dictionary)
-
+    mm = MmCorpus(inp)
+    dictionary = Dictionary.load_from_text(dictfile)
+    blei = BleiCorpus.serialize(outp, mm, dictionary)
 
 
