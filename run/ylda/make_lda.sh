5d721dceac7ddd629f340cecc5a237c2cd5504ba
kyhhdm <kyhhdm@gmail.com>
2015-09-17 16:08:58 -0400

    add chkinterval setting for make_lda.sh

--- run/ylda/make_lda.sh
+++ run/ylda/make_lda.sh
@@ -1,13 +1,23 @@
+#!/bin/bash
+
 ylda=/scratch/pengb/ylda
 work=/scratch/pengb/hpda/test/work
 
-if [ $# -eq "1" ]; then
+if [ $# -eq "1" -o $# -eq "2" ]; then
     echo "get server host from $1"
     hosts=(`cat $1`)
+
+    chkinterval=100
+    
+    if [ $# -eq "2" ]; then
+        chkinterval=$2
+    fi
 else
-    echo 'get server host from slaves'
-    hosts=(`cat slaves`)
+    echo "usage: make_lda.sh <slave file> <chkinterval>"
+    exit 
 fi
+
+echo "make run_lda.xxx scripts as: chkinterval=$chkinterval"
 
 topics=1000
 iter=1000
@@ -21,7 +31,7 @@ server_id=1
 for host in ${hosts[*]}; do
     echo "echo \"run learntopics on madrid-00$server_id\"" >  run_lda.madrid-00$server_id
     echo "cd $ylda && source $ylda/setLibVars.sh" >> run_lda.madrid-00$server_id
-    cmd="cd $work && nohup $ylda/learntopics --topics=$topics --iter=$iter --servers=$server_list --alpha=$alpha --beta=$beta  --optimizestats=5000 --samplerthreads=$threads --chkptdir="/tmp" --chkptinterval=10 &"
+    cmd="cd $work && nohup $ylda/learntopics --topics=$topics --iter=$iter --servers=$server_list --alpha=$alpha --beta=$beta  --optimizestats=5000 --samplerthreads=$threads --chkptdir="/tmp" --chkptinterval=$chkinterval &"
     echo $cmd >> run_lda.madrid-00$server_id
     ((server_id++))
 done
