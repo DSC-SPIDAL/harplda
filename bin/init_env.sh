dc3bb8e4a0dbca9a042b849d0cfeb6a17fdad98d
kyhhdm <kyhhdm@gmail.com>
2015-09-17 12:28:58 -0400

    update lda-testp

--- bin/init_env.sh
+++ bin/init_env.sh
@@ -11,5 +11,11 @@ echo 'Set _INSTDIR_ ->', $_INSTDIR_
 if [ ! $_OLD_PYTHONPATH ] ; then
     export _OLD_PYTHONPATH=$PYTHONPATH
 fi
+
 export PYTHONPATH=$_INSTDIR_/src:$_OLD_PYTHONPATH
+export PATH=$_INSTDIR_/bin:$PATH
+
+# add useful functions
+alias test_likelihood='python -m evaluation.test_likelihood'
+alias draw_likelihood='python -m evaluation.test_likelihood -draw'
 
