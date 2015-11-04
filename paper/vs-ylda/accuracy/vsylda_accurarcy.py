15f7accf1496ea5a71c200be7847e8766f957137
kyhhdm <kyhhdm@gmail.com>
2015-11-04 12:55:47 -0500

    add figs for vs-ylda experiments

--- paper/vs-ylda/accuracy/vsylda_accurarcy.py
+++ paper/vs-ylda/accuracy/vsylda_accurarcy.py
@@ -33,37 +33,37 @@ def draw_single():
     # clueweb
     ploter.init_subplot(1,1)
     ploter.set_subplot(1,1)
-    call_plot('accuracy_iter', 'data', 'accuracy_ib.clueweb', 'accuracy_iter_ib.clueweb.svg', "LDA Trainers Accuracy on Clueweb Dataset,IB")
+    call_plot('accuracy_iter', 'data', 'vsylda-accuracy.100-clueweb-ib', 'accuracy_iter_ib.clueweb.svg', "LDA Trainers Accuracy on Clueweb Dataset,IB")
     ploter.init_subplot(1,1)
     ploter.set_subplot(1,1)
  
-    call_plot('accuracy_runtime', 'data', 'accuracy_ib.clueweb', 'accuracy_runtime_ib.clueweb.svg', "LDA Trainers Accuracy on Clueweb Dataset,IB")
+    call_plot('accuracy_runtime', 'data', 'vsylda-accuracy.100-clueweb-ib', 'accuracy_runtime_ib.clueweb.svg', "LDA Trainers Accuracy on Clueweb Dataset,IB")
     ploter.init_subplot(1,1)
     ploter.set_subplot(1,1)
  
-    call_plot('accuracy_iter','data', 'accuracy_eth.clueweb', 'accuracy_iter_eth.clueweb.svg', "LDA Trainers Accuracy on Clueweb Dataset,Ethernet")
+    call_plot('accuracy_iter','data', 'vsylda-accuracy.100-clueweb-eth', 'accuracy_iter_eth.clueweb.svg', "LDA Trainers Accuracy on Clueweb Dataset,Ethernet")
     ploter.init_subplot(1,1)
     ploter.set_subplot(1,1)
  
-    call_plot('accuracy_runtime', 'data', 'accuracy_eth.clueweb', 'accuracy_runtime_eth.clueweb.svg', "LDA Trainers Accuracy on Clueweb Dataset,Ethernet")
+    call_plot('accuracy_runtime', 'data', 'vsylda-accuracy.100-clueweb-eth', 'accuracy_runtime_eth.clueweb.svg', "LDA Trainers Accuracy on Clueweb Dataset,Ethernet")
     ploter.init_subplot(1,1)
     ploter.set_subplot(1,1)
  
     
     # enwiki
-    call_plot('accuracy_iter','data','accuracy_ib.enwiki','accuracy_iter_ib.enwiki.svg',"LDA Trainers Accuracy on enwiki Dataset,IB")
+    call_plot('accuracy_iter','data','vsylda-accuracy.100-enwiki-ib','accuracy_iter_ib.enwiki.svg',"LDA Trainers Accuracy on enwiki Dataset,IB")
     ploter.init_subplot(1,1)
     ploter.set_subplot(1,1)
  
-    call_plot('accuracy_runtime', 'data','accuracy_ib.enwiki','accuracy_runtime_ib.enwiki.svg',"LDA Trainers Accuracy on enwiki Dataset,IB")
+    call_plot('accuracy_runtime', 'data','vsylda-accuracy.100-enwiki-ib','accuracy_runtime_ib.enwiki.svg',"LDA Trainers Accuracy on enwiki Dataset,IB")
     ploter.init_subplot(1,1)
     ploter.set_subplot(1,1)
         
-    call_plot('accuracy_iter', 'data','accuracy_eth.enwiki','accuracy_iter_eth.enwiki.svg',"LDA Trainers Accuracy on enwiki Dataset,Ethernet")
+    call_plot('accuracy_iter', 'data','vsylda-accuracy.100-enwiki-eth','accuracy_iter_eth.enwiki.svg',"LDA Trainers Accuracy on enwiki Dataset,Ethernet")
     ploter.init_subplot(1,1)
     ploter.set_subplot(1,1)
   
-    call_plot('accuracy_runtime', 'data','accuracy_eth.enwiki','accuracy_runtime_eth.enwiki.svg',"LDA Trainers Accuracy on enwiki Dataset,Ethernet")
+    call_plot('accuracy_runtime', 'data','vsylda-accuracy.100-enwiki-eth','accuracy_runtime_eth.enwiki.svg',"LDA Trainers Accuracy on enwiki Dataset,Ethernet")
 
 def draw_all():
     # output (2,2) subplots
@@ -72,13 +72,13 @@ def draw_all():
     ploter.fig.set_size_inches(9.25*1.5, 5.25*1.5)
 
     ploter.set_subplot(1,1)
-    call_plot('accuracy_iter', 'data', 'accuracy_ib.clueweb', '', "Clueweb Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_iter', 'data', 'vsylda-accuracy.100-clueweb-ib', '', "Clueweb Dataset, 16Gbps Infiniband")
     ploter.set_subplot(2,1)
-    call_plot('accuracy_runtime', 'data', 'accuracy_ib.clueweb', '', "Clueweb Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_runtime', 'data', 'vsylda-accuracy.100-clueweb-ib', '', "Clueweb Dataset, 16Gbps Infiniband")
     ploter.set_subplot(1,2)
-    call_plot('accuracy_iter','data', 'accuracy_eth.clueweb', '', "Clueweb Dataset, 1Gbps Ethernet")
+    call_plot('accuracy_iter','data', 'vsylda-accuracy.100-clueweb-eth', '', "Clueweb Dataset, 1Gbps Ethernet")
     ploter.set_subplot(2,2)
-    call_plot('accuracy_runtime', 'data', 'accuracy_eth.clueweb', '', "Clueweb Dataset, 1Gpbs Ethernet")
+    call_plot('accuracy_runtime', 'data', 'vsylda-accuracy.100-clueweb-eth', '', "Clueweb Dataset, 1Gpbs Ethernet")
     
     ploter.fig.subplots_adjust(hspace=0.4)
     ploter.savefig('accuracy_clueweb.svg')
@@ -89,13 +89,13 @@ def draw_all():
     ploter.fig.set_size_inches(9.25*1.5, 5.25*1.5)
  
     ploter.set_subplot(1,1)
-    call_plot('accuracy_iter','data','accuracy_ib.enwiki','',"Enwiki Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_iter','data','vsylda-accuracy.100-enwiki-ib','',"Enwiki Dataset, 16Gbps Infiniband")
     ploter.set_subplot(2,1)
-    call_plot('accuracy_runtime', 'data','accuracy_ib.enwiki','',"Enwiki Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_runtime', 'data','vsylda-accuracy.100-enwiki-ib','',"Enwiki Dataset, 16Gbps Infiniband")
     ploter.set_subplot(1,2)
-    call_plot('accuracy_iter', 'data','accuracy_eth.enwiki','',"Enwiki Dataset, 1Gbps Ethernet")
+    call_plot('accuracy_iter', 'data','vsylda-accuracy.100-enwiki-eth','',"Enwiki Dataset, 1Gbps Ethernet")
     ploter.set_subplot(2,2)
-    call_plot('accuracy_runtime', 'data','accuracy_eth.enwiki','',"Enwiki Dataset, 1Gbps Ethernet")
+    call_plot('accuracy_runtime', 'data','vsylda-accuracy.100-enwiki-eth','',"Enwiki Dataset, 1Gbps Ethernet")
 
     ploter.fig.subplots_adjust(hspace=0.4)
     ploter.savefig('accuracy_enwiki.svg')
@@ -107,9 +107,9 @@ def draw_enwiki_overall():
     ploter.fig.set_size_inches(9.25*1.5, 5.25*1.5)
 
     ploter.set_subplot(1,1)
-    call_plot('accuracy_overalltime', 'data', 'accuracy_ib.clueweb', '', "Clueweb Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_overalltime', 'data', 'vsylda-accuracy.100-clueweb-ib', '', "Clueweb Dataset, 16Gbps Infiniband")
     ploter.set_subplot(1,2)
-    call_plot('accuracy_overalltime', 'data', 'accuracy_eth.clueweb', '', "Clueweb Dataset, 1Gpbs Ethernet")
+    call_plot('accuracy_overalltime', 'data', 'vsylda-accuracy.100-clueweb-eth', '', "Clueweb Dataset, 1Gpbs Ethernet")
     
     ploter.fig.subplots_adjust(hspace=0.4)
     ploter.savefig('accuracy_clueweb_excutiontime.svg')
@@ -120,9 +120,9 @@ def draw_enwiki_overall():
     ploter.fig.set_size_inches(9.25*1.5, 5.25*1.5)
  
     ploter.set_subplot(1,1)
-    call_plot('accuracy_overalltime', 'data','accuracy_ib.enwiki','',"Enwiki Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_overalltime', 'data','vsylda-accuracy.100-enwiki-ib','',"Enwiki Dataset, 16Gbps Infiniband")
     ploter.set_subplot(1,2)
-    call_plot('accuracy_overalltime', 'data','accuracy_eth.enwiki','',"Enwiki Dataset, 1Gbps Ethernet")
+    call_plot('accuracy_overalltime', 'data','vsylda-accuracy.100-enwiki-eth','',"Enwiki Dataset, 1Gbps Ethernet")
 
     ploter.fig.subplots_adjust(hspace=0.4)
     ploter.savefig('accuracy_enwiki_excutiontime.svg')
@@ -134,10 +134,10 @@ def draw_overall_30():
     ploter.fig.set_size_inches(9.25*1.5, 5.25*1.5)
 
     ploter.set_subplot(1,1)
-    call_plot('accuracy_overalltime', 'data', 'accuracy_30_ib.clueweb', '', "Clueweb Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_overalltime', 'data', 'vsylda-accuracy.30-clueweb-ib', '', "Clueweb Dataset, 16Gbps Infiniband")
     ploter.set_subplot(1,2)
 #call_plot('accuracy_overalltime', 'data', 'accuracy_30_eth.clueweb', '', "Clueweb Dataset, 1Gpbs Ethernet")
-    call_plot('accuracy_overalltime', 'data', 'accuracy_30_ib.clueweb', '', "Clueweb Dataset, 1Gpbs Ethernet")
+    call_plot('accuracy_overalltime', 'data', 'vsylda-accuracy.30-clueweb-ib', '', "Clueweb Dataset, 1Gpbs Ethernet")
     
     ploter.fig.subplots_adjust(hspace=0.4)
     ploter.savefig('accuracy_30_clueweb_excutiontime.svg')
@@ -148,10 +148,10 @@ def draw_overall_30():
     ploter.fig.set_size_inches(9.25*1.5, 5.25*1.5)
  
     ploter.set_subplot(1,1)
-    call_plot('accuracy_overalltime', 'data','accuracy_30_ib.enwiki','',"Enwiki Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_overalltime', 'data','vsylda-accuracy.30-enwiki-ib','',"Enwiki Dataset, 16Gbps Infiniband")
     ploter.set_subplot(1,2)
 #call_plot('accuracy_overalltime', 'data','accuracy_30_eth.enwiki','',"Enwiki Dataset, 1Gbps Ethernet")
-    call_plot('accuracy_overalltime', 'data','accuracy_30_ib.enwiki','',"Enwiki Dataset, 1Gbps Ethernet")
+    call_plot('accuracy_overalltime', 'data','vsylda-accuracy.30-enwiki-ib','',"Enwiki Dataset, 1Gbps Ethernet")
 
     ploter.fig.subplots_adjust(hspace=0.4)
     ploter.savefig('accuracy_30_enwiki_excutiontime.svg')
@@ -164,9 +164,9 @@ def draw_30_enwiki_all():
 
 
     ploter.set_subplot(1,1)
-    call_plot('accuracy_iter', 'data','accuracy_30_ib.enwiki','',"Enwiki Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_iter', 'data','vsylda-accuracy.30-enwiki-ib','',"Enwiki Dataset, 16Gbps Infiniband")
     ploter.set_subplot(2,1)
-    call_plot('accuracy_runtime', 'data','accuracy_30_ib.enwiki','',"Enwiki Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_runtime', 'data','vsylda-accuracy.30-enwiki-ib','',"Enwiki Dataset, 16Gbps Infiniband")
 
     ploter.fig.subplots_adjust(hspace=0.4)
     ploter.savefig('accuracy_enwiki_30.svg')
--- paper/vs-ylda/comm/vsylda_comm.py
+++ paper/vs-ylda/comm/vsylda_comm.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Plot the accuracy performance firugres on performance data
+Plot the comm/computation time breakwodn
 
 """
 
@@ -32,38 +32,26 @@ def call_plot(plotname, datadir, namefile, figname, title):
 def draw_single():
     # clueweb
     ploter.init_subplot(1,1)
+    ploter.fig.set_size_inches(9.25*1.5, 5.25*1.5)
     ploter.set_subplot(1,1)
-    call_plot('accuracy_iter', 'data', 'accuracy_ib.clueweb', 'accuracy_iter_ib.clueweb.svg', "LDA Trainers Accuracy on Clueweb Dataset,IB")
+    call_plot('comm_breakdown', 'data', 'vsylda-accuracy.100-clueweb-ib', 'comm_breakdown_ib.clueweb.svg', "Comm/Computation time breakdown on Clueweb Dataset,IB")
+
     ploter.init_subplot(1,1)
+    ploter.fig.set_size_inches(9.25*1.5, 5.25*1.5)
     ploter.set_subplot(1,1)
- 
-    call_plot('accuracy_runtime', 'data', 'accuracy_ib.clueweb', 'accuracy_runtime_ib.clueweb.svg', "LDA Trainers Accuracy on Clueweb Dataset,IB")
-    ploter.init_subplot(1,1)
-    ploter.set_subplot(1,1)
- 
-    call_plot('accuracy_iter','data', 'accuracy_eth.clueweb', 'accuracy_iter_eth.clueweb.svg', "LDA Trainers Accuracy on Clueweb Dataset,Ethernet")
-    ploter.init_subplot(1,1)
-    ploter.set_subplot(1,1)
- 
-    call_plot('accuracy_runtime', 'data', 'accuracy_eth.clueweb', 'accuracy_runtime_eth.clueweb.svg', "LDA Trainers Accuracy on Clueweb Dataset,Ethernet")
-    ploter.init_subplot(1,1)
-    ploter.set_subplot(1,1)
- 
-    
+    call_plot('comm_breakdown','data', 'vsylda-accuracy.100-clueweb-eth', 'comm_breakdown_eth.clueweb.svg', "Comm/Computation time breakdown on Clueweb Dataset,Ethernet")
+
     # enwiki
-    call_plot('accuracy_iter','data','accuracy_ib.enwiki','accuracy_iter_ib.enwiki.svg',"LDA Trainers Accuracy on enwiki Dataset,IB")
     ploter.init_subplot(1,1)
+    ploter.fig.set_size_inches(9.25*1.5, 5.25*1.5)
     ploter.set_subplot(1,1)
- 
-    call_plot('accuracy_runtime', 'data','accuracy_ib.enwiki','accuracy_runtime_ib.enwiki.svg',"LDA Trainers Accuracy on enwiki Dataset,IB")
+    call_plot('comm_breakdown', 'data', 'vsylda-accuracy.100-enwiki-ib', 'comm_breakdown_ib.enwiki.svg', "Comm/Computation time breakdown on enwiki Dataset,IB")
+
     ploter.init_subplot(1,1)
+    ploter.fig.set_size_inches(9.25*1.5, 5.25*1.5)
     ploter.set_subplot(1,1)
-        
-    call_plot('accuracy_iter', 'data','accuracy_eth.enwiki','accuracy_iter_eth.enwiki.svg',"LDA Trainers Accuracy on enwiki Dataset,Ethernet")
-    ploter.init_subplot(1,1)
-    ploter.set_subplot(1,1)
-  
-    call_plot('accuracy_runtime', 'data','accuracy_eth.enwiki','accuracy_runtime_eth.enwiki.svg',"LDA Trainers Accuracy on enwiki Dataset,Ethernet")
+    call_plot('comm_breakdown','data', 'vsylda-accuracy.100-enwiki-eth', 'comm_breakdown_eth.enwiki.svg', "Comm/Computation time breakdown on enwiki Dataset,Ethernet")
+
 
 def draw_all():
     # output (2,2) subplots
@@ -72,13 +60,13 @@ def draw_all():
     ploter.fig.set_size_inches(9.25*1.5, 5.25*1.5)
 
     ploter.set_subplot(1,1)
-    call_plot('accuracy_iter', 'data', 'accuracy_ib.clueweb', '', "Clueweb Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_iter', 'data', 'vsylda-accuracy.100-clueweb-ib', '', "Clueweb Dataset, 16Gbps Infiniband")
     ploter.set_subplot(2,1)
-    call_plot('accuracy_runtime', 'data', 'accuracy_ib.clueweb', '', "Clueweb Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_runtime', 'data', 'vsylda-accuracy.100-clueweb-ib', '', "Clueweb Dataset, 16Gbps Infiniband")
     ploter.set_subplot(1,2)
-    call_plot('accuracy_iter','data', 'accuracy_eth.clueweb', '', "Clueweb Dataset, 1Gbps Ethernet")
+    call_plot('accuracy_iter','data', 'vsylda-accuracy.100-clueweb-eth', '', "Clueweb Dataset, 1Gbps Ethernet")
     ploter.set_subplot(2,2)
-    call_plot('accuracy_runtime', 'data', 'accuracy_eth.clueweb', '', "Clueweb Dataset, 1Gpbs Ethernet")
+    call_plot('accuracy_runtime', 'data', 'vsylda-accuracy.100-clueweb-eth', '', "Clueweb Dataset, 1Gpbs Ethernet")
     
     ploter.fig.subplots_adjust(hspace=0.4)
     ploter.savefig('accuracy_clueweb.svg')
@@ -89,13 +77,13 @@ def draw_all():
     ploter.fig.set_size_inches(9.25*1.5, 5.25*1.5)
  
     ploter.set_subplot(1,1)
-    call_plot('accuracy_iter','data','accuracy_ib.enwiki','',"Enwiki Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_iter','data','vsylda-accuracy.100-enwiki-ib','',"Enwiki Dataset, 16Gbps Infiniband")
     ploter.set_subplot(2,1)
-    call_plot('accuracy_runtime', 'data','accuracy_ib.enwiki','',"Enwiki Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_runtime', 'data','vsylda-accuracy.100-enwiki-ib','',"Enwiki Dataset, 16Gbps Infiniband")
     ploter.set_subplot(1,2)
-    call_plot('accuracy_iter', 'data','accuracy_eth.enwiki','',"Enwiki Dataset, 1Gbps Ethernet")
+    call_plot('accuracy_iter', 'data','vsylda-accuracy.100-enwiki-eth','',"Enwiki Dataset, 1Gbps Ethernet")
     ploter.set_subplot(2,2)
-    call_plot('accuracy_runtime', 'data','accuracy_eth.enwiki','',"Enwiki Dataset, 1Gbps Ethernet")
+    call_plot('accuracy_runtime', 'data','vsylda-accuracy.100-enwiki-eth','',"Enwiki Dataset, 1Gbps Ethernet")
 
     ploter.fig.subplots_adjust(hspace=0.4)
     ploter.savefig('accuracy_enwiki.svg')
@@ -107,9 +95,9 @@ def draw_enwiki_overall():
     ploter.fig.set_size_inches(9.25*1.5, 5.25*1.5)
 
     ploter.set_subplot(1,1)
-    call_plot('accuracy_overalltime', 'data', 'accuracy_ib.clueweb', '', "Clueweb Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_overalltime', 'data', 'vsylda-accuracy.100-clueweb-ib', '', "Clueweb Dataset, 16Gbps Infiniband")
     ploter.set_subplot(1,2)
-    call_plot('accuracy_overalltime', 'data', 'accuracy_eth.clueweb', '', "Clueweb Dataset, 1Gpbs Ethernet")
+    call_plot('accuracy_overalltime', 'data', 'vsylda-accuracy.100-clueweb-eth', '', "Clueweb Dataset, 1Gpbs Ethernet")
     
     ploter.fig.subplots_adjust(hspace=0.4)
     ploter.savefig('accuracy_clueweb_excutiontime.svg')
@@ -120,9 +108,9 @@ def draw_enwiki_overall():
     ploter.fig.set_size_inches(9.25*1.5, 5.25*1.5)
  
     ploter.set_subplot(1,1)
-    call_plot('accuracy_overalltime', 'data','accuracy_ib.enwiki','',"Enwiki Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_overalltime', 'data','vsylda-accuracy.100-enwiki-ib','',"Enwiki Dataset, 16Gbps Infiniband")
     ploter.set_subplot(1,2)
-    call_plot('accuracy_overalltime', 'data','accuracy_eth.enwiki','',"Enwiki Dataset, 1Gbps Ethernet")
+    call_plot('accuracy_overalltime', 'data','vsylda-accuracy.100-enwiki-eth','',"Enwiki Dataset, 1Gbps Ethernet")
 
     ploter.fig.subplots_adjust(hspace=0.4)
     ploter.savefig('accuracy_enwiki_excutiontime.svg')
@@ -134,10 +122,10 @@ def draw_overall_30():
     ploter.fig.set_size_inches(9.25*1.5, 5.25*1.5)
 
     ploter.set_subplot(1,1)
-    call_plot('accuracy_overalltime', 'data', 'accuracy_30_ib.clueweb', '', "Clueweb Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_overalltime', 'data', 'vsylda-accuracy.30-clueweb-ib', '', "Clueweb Dataset, 16Gbps Infiniband")
     ploter.set_subplot(1,2)
 #call_plot('accuracy_overalltime', 'data', 'accuracy_30_eth.clueweb', '', "Clueweb Dataset, 1Gpbs Ethernet")
-    call_plot('accuracy_overalltime', 'data', 'accuracy_30_ib.clueweb', '', "Clueweb Dataset, 1Gpbs Ethernet")
+    call_plot('accuracy_overalltime', 'data', 'vsylda-accuracy.30-clueweb-ib', '', "Clueweb Dataset, 1Gpbs Ethernet")
     
     ploter.fig.subplots_adjust(hspace=0.4)
     ploter.savefig('accuracy_30_clueweb_excutiontime.svg')
@@ -148,10 +136,10 @@ def draw_overall_30():
     ploter.fig.set_size_inches(9.25*1.5, 5.25*1.5)
  
     ploter.set_subplot(1,1)
-    call_plot('accuracy_overalltime', 'data','accuracy_30_ib.enwiki','',"Enwiki Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_overalltime', 'data','vsylda-accuracy.30-enwiki-ib','',"Enwiki Dataset, 16Gbps Infiniband")
     ploter.set_subplot(1,2)
 #call_plot('accuracy_overalltime', 'data','accuracy_30_eth.enwiki','',"Enwiki Dataset, 1Gbps Ethernet")
-    call_plot('accuracy_overalltime', 'data','accuracy_30_ib.enwiki','',"Enwiki Dataset, 1Gbps Ethernet")
+    call_plot('accuracy_overalltime', 'data','vsylda-accuracy.30-enwiki-ib','',"Enwiki Dataset, 1Gbps Ethernet")
 
     ploter.fig.subplots_adjust(hspace=0.4)
     ploter.savefig('accuracy_30_enwiki_excutiontime.svg')
@@ -164,9 +152,9 @@ def draw_30_enwiki_all():
 
 
     ploter.set_subplot(1,1)
-    call_plot('accuracy_iter', 'data','accuracy_30_ib.enwiki','',"Enwiki Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_iter', 'data','vsylda-accuracy.30-enwiki-ib','',"Enwiki Dataset, 16Gbps Infiniband")
     ploter.set_subplot(2,1)
-    call_plot('accuracy_runtime', 'data','accuracy_30_ib.enwiki','',"Enwiki Dataset, 16Gbps Infiniband")
+    call_plot('accuracy_runtime', 'data','vsylda-accuracy.30-enwiki-ib','',"Enwiki Dataset, 16Gbps Infiniband")
 
     ploter.fig.subplots_adjust(hspace=0.4)
     ploter.savefig('accuracy_enwiki_30.svg')
@@ -184,9 +172,3 @@ if __name__ == "__main__":
     logger.info("running %s" % ' '.join(sys.argv))
 
     draw_single()
-    draw_all()
-    draw_enwiki_overall()
-
-    draw_overall_30()
-
-    draw_30_enwiki_all()
