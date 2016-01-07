68d95e4deaa172c4117c4b9093d8289b4a97f166
kyhhdm <kyhhdm@gmail.com>
2016-01-07 12:02:53 -0500

    update netflix.py by create train,test set by probe.txt

--- src/preprocess/netflix.py
+++ src/preprocess/netflix.py
@@ -5,7 +5,7 @@
 Convert netflix dataset to simple MM format
 
 Simple Matrix Market Format:
-    #dataset.mm
+    #dataset-[train,test].mm
     rowid   colid   val
 
     #dataset.rfreq
@@ -18,7 +18,7 @@ Simple Matrix Market Format:
     rowcnt  colcnt  nonzerocnt
 
 Usage:
-    netflix.py  <datadir> <output>
+    netflix.py -[all|judge|train] <datadir> <output>
 
 """
 
@@ -48,6 +48,22 @@ class NetflixDataset():
     - Ratings are on a five star (integral) scale from 1 to 5.
     - Dates have the format YYYY-MM-DD.
 
+
+    THE PROBE DATASET FILE DESCRIPTION
+    ==================================
+    To allow you to test your system before you submit a prediction set based on the
+    qualifying dataset, we have provided a probe dataset in the file "probe.txt".
+    This text file contains lines indicating a movie id, followed by a colon, and
+    then customer ids, one per line for that movie id.
+
+    MovieID1:
+    CustomerID11
+    CustomerID12
+    ...
+    MovieID3:
+    CustomerID22
+    CustomerID22
+
     """
     def __init__(self, datadir):
         self.rowids = {}
@@ -55,6 +71,8 @@ class NetflixDataset():
         self.nnz = 0
          
         self.tar = tarfile.open(datadir + '/training_set.tar')
+        self.probef = open(datadir + '/probe.txt')
+
 
         self.testf = open(datadir + '/qualifying.txt')
         self.judgef = open(datadir + '/judging.txt')
@@ -62,6 +80,9 @@ class NetflixDataset():
     def convert_test(self, mmfile):
         """
         convert test file to mm format
+    
+        ultimate test file is seperate to two files: qualifying.txt and judging.txt
+
         """
         targetfile = mmfile + '.mm'
         if os.path.exists(targetfile):
@@ -97,10 +118,82 @@ class NetflixDataset():
         statusf.write('%d\n'%(len(testset)))
         statusf.close()
 
+    def convert_train(self, mmfile):
+        """
+        convert to mm format
+
+        separate the whole dataset by probe.txt as testset, remaining as traingset
+        """
+        targetfile = mmfile + '-train.mm'
+        if os.path.exists(targetfile):
+            logger.info('%s file exist already, skip overwriting', targetfile)
+            return
+
+        # load probe.txt as testset
+        testmap={}
+        for line in self.probef:
+            if line.find(':') > 0:
+                movieid = line.split(':')[0]
+            else:
+                userid = line.strip()
+                testmap[(userid,movieid)] = 1
+ 
+        mmf_train = open(mmfile + '-train.mm', 'w', 1024*1024)
+        mmf_test = open(mmfile + '-test.mm', 'w', 1024*1024)
+
+        for tar_info in self.tar:
+            if not tar_info.isreg():
+                continue
+            file = self.tar.extractfile(tar_info)
+            firstline = file.readline()
+            movieid = firstline.split(':')[0]
+            for line in file:
+                items = line.split(',')
+                userid = items[0]
+                val = items[1]
+
+                if (userid, movieid) in testmap:
+                    mmf_test.write('%s %s %s\n'%(userid, movieid, val))
+                else:
+                    mmf_train.write('%s %s %s\n'%(userid, movieid, val))
+
+                # statistics
+                self.nnz += 1
+                if userid in self.rowids:
+                    self.rowids[userid] += 1
+                else:
+                    self.rowids[userid] = 1
+
+                if movieid in self.colids:
+                    self.colids[movieid] += 1
+                else:
+                    self.colids[movieid] = 1
+
+        mmf_train.close()
+        mmf_test.close()
+
+        # write freqfile
+        freqf = open(mmfile + '.rfreq', 'w', 1024*1024)
+        sort_rowids = sorted(self.rowids.items(), key = lambda tp : tp[1], reverse=True)
+        for tp in sort_rowids:
+            freqf.write('%s %d\n'%(tp[0], tp[1]))
+        freqf.close()
+
+        freqf = open(mmfile + '.cfreq', 'w', 1024*1024)
+        sort_colids = sorted(self.colids.items(), key = lambda tp : tp[1], reverse = True)
+        for tp in sort_colids:
+            freqf.write('%s %d\n'%(tp[0], tp[1]))
+        freqf.close()
+
+        statusf = open(mmfile + '.status', 'w')
+        statusf.write('%d %d %d\n'%(len(self.rowids), len(self.colids), self.nnz))
+        statusf.close()
+
 
     def convert(self, mmfile):
         """
         convert to mm format
+
 
         """
         targetfile = mmfile + '.mm'
@@ -169,15 +262,21 @@ if __name__ == '__main__':
     logger.info("running %s" % ' '.join(sys.argv))
 
     # check and process input arguments
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 4:
         print(globals()['__doc__'] % locals())
         sys.exit(1)
 
-    datadir = sys.argv[1]
-    mmfile = sys.argv[2]
+    datadir = sys.argv[2]
+    mmfile = sys.argv[3]
 
     netflix = NetflixDataset(datadir)
-    netflix.convert(mmfile)
+
+    cmd = sys.argv[1]
+    if cmd == '-all':
+        netflix.convert(mmfile + '-all')
+    elif cmd == '-judge':
+        netflix.convert_test(mmfile + '-judge')
+    else:
+        netflix.convert_train(mmfile)
     logger.info('%s', netflix)
-    netflix.convert_test(mmfile + '-test')
 
